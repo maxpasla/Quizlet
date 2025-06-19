69de3e00d02d8b112e6a0d1574c722feae0bb2dd
Maxence Roger <maxenceroger3.5@gmail.com>
2025-06-19 02:00:10 +0200

    Ajout de main.tsx et correction structure Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
+// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  root: 'public',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
